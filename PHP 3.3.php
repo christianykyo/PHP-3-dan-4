d5f113a61a94924f5ca0e4579a7e4542ed117b69
christianykyo <53499637+christianykyo@users.noreply.github.com>
2020-03-09 09:40:34 +0700

    Add files via upload

---
+++ PHP 3.3.php
@@ -0,0 +1,9 @@
+<?php
+    $user = "Chris";
+    $pass = "333";
+    if ($user == "Chris" && $pass == "333") {
+        echo "Login Berhasil";
+    } else {
+        echo "Login Gagal";
+    }
+?>
---
+++ PHP 3.4.php
@@ -0,0 +1,9 @@
+<?php
+    $user = "Chris";
+    if (!isset($user)) {
+        echo "Variabel tidak ada.";
+    }
+    else {
+        echo "Variabel ada.";
+    }
+?>
