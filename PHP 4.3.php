0204ed62ad75c58c28d24e987b1564e957921881
christianykyo <53499637+christianykyo@users.noreply.github.com>
2020-03-09 09:47:24 +0700

    Add files via upload

---
+++ PHP 4.3.php
@@ -0,0 +1,8 @@
+<?php
+    function head() {
+        echo `
+        <link rel="stylesheet" type="text/css" href="style/stylecss">
+        <link rel="stylesheet" type="text/css" href="style/font-awesome.css> `;
+    }
+    echo luas_lingkaran($r);
+?>
---
+++ PHP 4.php
@@ -0,0 +1,11 @@
+<?php
+
+    function perkenalan() {
+    echo "Shalom, ";
+    echo "Perkenalkan, nama saya Chris<br/>";
+    echo "Senang berkenalan denganmu.<br/>";
+    }
+    perkenalan();
+        echo "<hr>";
+    perkenalan();
+?>
